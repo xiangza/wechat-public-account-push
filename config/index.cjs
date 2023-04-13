3192e1e85ddab068a88e16de5546d09b23f67eb5
xiangza <83255336+xiangza@users.noreply.github.com>
2023-04-13 10:01:06 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -47,6 +47,36 @@ const USER_CONFIG = {
         //{ keyword: 'marry_day', date: '2022-09-09' },
       ],
     },
+    {
+      // 想要发送的人的名字
+      name: '武林权',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'oXqBw6aU5mKtWrNBoF_X43AImITg',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: 'DCiQxj0A_w9ZiZw4nqkdCsXB8KFAyDaWnmIQ9mvSq0Q',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '3-22',
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '*生日', name: '武林权', year: '2002', date: '02-22',
+        },
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        {
+          type: '生日', name: '郭涛', year: '2002', date: '09-31',
+        },
+        {
+          type: '节日', name: '相识纪念日', year: '2020', date: '07-11',
+        },
+      ],
+      // 我们在一起已经有xxxx天了的配置
+      customizedDateList: [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2021-11-16' },
+        // 结婚纪念日
+        //{ keyword: 'marry_day', date: '2022-09-09' },
+      ],
+    },
   ],
 
 
